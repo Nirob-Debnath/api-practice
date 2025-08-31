606deb5c9897510c7986abca312eb8c396f860aa
unknown <nirobdebnath456@gmail.com>
2025-09-01 01:56:28 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const handleLoadUser = () => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.error('Error fetching user:', error);
+        });
+}
